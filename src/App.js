7f1cb9022d69259b451dba65d561bca1212f18eb
Muhammed Sabbah <30741844+muhammedSabbah@users.noreply.github.com>
2020-05-02 18:21:34 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+    import React, { Component } from 'react';
+    import logo from './logo.svg';
+    import './App.css';
+     
+    class App extends Component {
+      render() {
+        return (
+          <div className="App">
+            <h1 className="hello">Hello World</h1>
+            <FirstComponent></FirstComponent>
+            <SecondComponent></SecondComponent>
+            <ThirdComponent></ThirdComponent>
+          </div>
+        );
+      }
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    class FirstComponent extends Component {
+      render() {
+        return (
+          <div className="firstComponent">
+            First Component
+          </div>
+        );
+      }
+    }
 
-export default App;
+    class SecondComponent extends Component {
+      render() {
+        return (
+          <div className="secondComponent">
+            Second Component
+          </div>
+        );
+      }
+    }
+
+    function ThirdComponent() {
+      return (
+        <div className="thirdComponent">
+          Third Component
+        </div>
+      );
+    }
+
+    export default App;
+
+    /*
+    <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload...
+            </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React With Me
+            </a>
+            </header>
+    */
